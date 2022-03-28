507446f3f468f4f9b007b30f6425f3350000238f
PK <madigani.prasannakumar13@gmail.com>
2022-03-29 02:34:12 +0530

    Initial commit

---
+++ FuzzyLayer.py
@@ -0,0 +1,34 @@
+
+from keras import backend as K
+from tensorflow.keras.layers import Layer
+
+
+class FuzzyLayer(Layer):
+
+    def __init__(self, output_dim, initialiser_centers=None, initialiser_sigmas=None, **kwargs):
+        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+            kwargs['input_shape'] = (kwargs.pop('input_dim'),)
+        self.output_dim = output_dim
+        self.initialiser_centers = initialiser_centers
+        self.initialiser_sigmas = initialiser_sigmas
+        super(FuzzyLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.fuzzy_degree = self.add_weight(name='fuzzy_degree',
+                                            shape=(input_shape[-1], self.output_dim),
+                                            initializer=self.initialiser_centers if self.initialiser_centers is not None else 'uniform',
+                                            trainable=True)
+        self.sigma = self.add_weight(name='sigma',
+                                     shape=(input_shape[-1], self.output_dim),
+                                     initializer=self.initialiser_sigmas if self.initialiser_sigmas is not None else 'ones',
+                                     trainable=True)
+        super(FuzzyLayer, self).build(input_shape)
+
+    def call(self, input, **kwargs):
+        x = K.repeat_elements(K.expand_dims(input, axis=-1), self.output_dim, -1)
+
+        fuzzy_out = K.exp(-K.sum(K.square((x - self.fuzzy_degree) / (self.sigma ** 2)), axis=-2, keepdims=False))
+        return fuzzy_out
+
+    def compute_output_shape(self, input_shape):
+        return tuple(input_shape[:-1]) + (self.output_dim,)
---
+++ deep-learning-genomics-primer-master/deep-learning-genomics-primer-master/README.md
@@ -0,0 +1,18 @@
+# Deep Learning Genomics Primer
+
+This tutorial is a supplement to the manuscript, **A Primer on Deep Learning in Genomics** (*Nature Genetics, 2018*) by James Zou, Mikael Huss, Abubakar Abid, Pejman Mohammadi, Ali Torkamani & Amalio Telentil. [Read the accompanying paper here](https://www.nature.com/articles/s41588-018-0295-5).
+
+See the associated [python notebook](https://github.com/abidlabs/deep-learning-genomics-primer/blob/master/A_Primer_on_Deep_Learning_in_Genomics_Public.ipynb) for the tutorial, or run it right from your browser in a [colab notebook](https://colab.research.google.com/drive/17E4h5aAOioh5DiTo7MZg4hpL6Z_0FyWr#scrollTo=eiiwjw4yhX0P).
+
+  If you have any questions or feedback regarding this tutorial, please contact Abubakar Abid <<a12d@stanford.edu>> or James Zou <<jamesz@stanford.edu>>. Please cite using the bibtex below:
+
+```
+@article{zou2018primer,
+  title={A primer on deep learning in genomics},
+  author={Zou, James and Huss, Mikael and Abid, Abubakar and Mohammadi, Pejman and Torkamani, Ali and Telenti, Amalio},
+  journal={Nature genetics},
+  pages={1},
+  year={2018},
+  publisher={Nature Publishing Group}
+}
+```
